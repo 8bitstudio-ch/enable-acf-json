32ef9932b1e9179dc45dd2d950fbf503bbc29bbc
Mathieu Zwygart lol <mathieu@zwygart.net>
2023-03-29 14:37:10 +0200

    Refactor

--- enable-acf-json.php
+++ enable-acf-json.php
@@ -3,31 +3,36 @@
 Plugin Name:  Enable ACF Local JSON
 Plugin URI:   https://8bitstudio.ch
 Description:  Saves field group and field settings as .json files in /app/json folder
-Version:      1.0.0
+Version:      1.2.0
 Author:       Mathieu Zwygart
 Author URI:   https://zwygart.net
 License:      MIT License
 */
 
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
- 
-function my_acf_json_save_point( $path ) {  
-	// update path
-	$path = dirname(__DIR__, 2) . '/acf-json';
+class EnableACFJSON {
+	public function __construct() {
+		$this->json_directory_path = dirname(__DIR__, 2) . '/acf-json';
+	}
+
+	public function run() {
+		if (!file_exists($this->json_directory_path)) {
+			mkdir($this->json_directory_path, 0777, true);
+		}
+
+		add_filter('acf/settings/save_json', $this->json_directory_path);
+		add_filter('acf/settings/load_json', function ($paths) {
+			unset($paths[0]);
 	
-	// return
-	return $path;
+			$paths[] = $this->json_directory_path;
+	
+			return $paths;
+		});
+	}
 }
 
-add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-
-function my_acf_json_load_point( $paths ) {
-	// remove original path (optional)
-	unset($paths[0]);
-
-	// append path
-	$paths[] = dirname(__DIR__, 2) . '/acf-json';
-
-	// return
-	return $paths;
+function run_enable_acf_json() {
+	$plugin = new EnableACFJSON();
+	$plugin->run();
 }
+
+run_enable_acf_json();
