511d3a31a521e0758fda0a26ac2b580f5228a270
gutembergg <gmascarenhas3001@gmail.com>
2023-04-09 12:32:31 +0200

    v2 changes

--- enable-acf-json.php
+++ enable-acf-json.php
@@ -2,37 +2,43 @@
 /*
 Plugin Name:  Enable ACF Local JSON
 Plugin URI:   https://8bitstudio.ch
-Description:  Saves field group and field settings as .json files in /app/acf-json folder
-Version:      1.2.0
+Description:  Saves field group and field settings as .json files in /app/json folder
+Version:      1.0.0
 Author:       Mathieu Zwygart
 Author URI:   https://zwygart.net
 License:      MIT License
 */
 
-class EnableACFJSON {
-	public function __construct() {
-		$this->json_directory_path = dirname(__DIR__, 2) . '/acf-json';
+use function Env\env;
+
+$path = dirname(__DIR__, 2) . '/acf-json';
+if ( wp_mkdir_p( $path ) ) {
+	
+	if(env('WP_ENV') == 'production') {
+		add_filter('acf/settings/show_admin', '__return_false');
 	}
 
-	public function run() {
-		if (!file_exists($this->json_directory_path)) {
-			mkdir($this->json_directory_path, 0777, true);
+   add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+		function my_acf_json_save_point( $path ) {  
+			// update path
+			$path = dirname(__DIR__, 2) . '/acf-json';
+
+
+			// return
+			return $path;
 		}
 
-		add_filter('acf/settings/save_json', $this->json_directory_path);
-		add_filter('acf/settings/load_json', function ($paths) {
+		add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+		function my_acf_json_load_point( $paths ) {
+			// remove original path (optional)
 			unset($paths[0]);
-	
-			$paths[] = $this->json_directory_path;
-	
+
+			// append path
+			$paths[] = dirname(__DIR__, 2) . '/acf-json';
+
+			// return
 			return $paths;
-		});
-	}
+		}
 }
-
-function run_enable_acf_json() {
-	$plugin = new EnableACFJSON();
-	$plugin->run();
-}
-
-run_enable_acf_json();
